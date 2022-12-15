79acf870b81c8aa9d6aee49ea41a7dbe2a2adbe2
Shahanaj2000 <shahanajmt@gmail.com>
2022-12-15 17:46:57 +0530

    Form Key Validation

--- lib/Screens/Login_Screen.dart
+++ lib/Screens/Login_Screen.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class ScreenLogin extends StatelessWidget {
+class ScreenLogin extends StatefulWidget {
   ScreenLogin({super.key});
 
+  @override
+  State<ScreenLogin> createState() => _ScreenLoginState();
+}
+
+class _ScreenLoginState extends State<ScreenLogin> {
    //For geting the text into DB
     final _usernameController = TextEditingController();
+
     final _passwordController = TextEditingController();
+
+    bool _isDataMatched = true;
+
+    final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -21,41 +31,89 @@ class ScreenLogin extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-               TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Username',
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                 TextFormField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Username',
+                  ),
+                  validator: (value) {
+                    /*if(_isDataMatched) {
+                      return null;
+                    } else {
+                      return "Username or Password doesn't match";
+                    }*/
+                    if (value == null || value.isEmpty) {
+                      return 'Value is empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                ), 
+                const SizedBox(
+                  height: 10,
                 ),
-              ), 
-              const SizedBox(
-                height: 10,
-              ),
-               TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Password'
+                 TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Password'
+                  ),
+                  validator: (value) {
+                    /*if(_isDataMatched) {
+                      return null;
+                    } else {
+                      return "Username or Password doesn't match";
+                    } */
+                    if (value == null || value.isEmpty) {
+                      return 'Value is empty';
+                    } else {
+                      return null;
+                    }
+                  },
+                    
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton.icon(
-                onPressed: () { checkLogin(context);},
-                icon: const Icon(Icons.check),
-                label: const Text("LOGIN"),
-              ),
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Visibility(
+                      visible: !_isDataMatched,
+                      child: const Text(
+                        "Username or Password doesn't match",
+                      style: TextStyle(color: Colors.red),), 
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () { 
+                        checkLogin(context);
+                        if(_formkey.currentState!.validate()) {
+                          return checkLogin(context);
+                        } else {
+                          print("Data Empty");
+                        }
+                        
+                      },
+                      icon: const Icon(Icons.check),
+                      label: const Text("LOGIN"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
   void checkLogin(BuildContext ctx) {
     final _password = _usernameController.text;
     final _username = _passwordController.text;
@@ -63,35 +121,7 @@ class ScreenLogin extends StatelessWidget {
     if (_username == _password) {
       //Go to Home page
     } else {
-      final _errorMessage = "Username or Password doesn't mathch";
-
-      //Snack Bars
-      ScaffoldMessenger.of(ctx).showSnackBar(
-        SnackBar(
-          content: Text(_errorMessage),
-          behavior: SnackBarBehavior.floating,
-          margin: const EdgeInsets.all(20.0),
-          backgroundColor: Colors.red,
-          duration: const Duration(seconds: 10),
-        ),
-      );
-
-      //Alert
-
-      showDialog(context: ctx, builder: (ctx1) {
-        return AlertDialog(
-          title: const Text('Eroor'),
-          content: Text(_errorMessage),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(ctx1).pop();
-              },
-              child: const Text("Close"),
-            ),
-          ], 
-        );
-      });
+      
     }
   }
 }
